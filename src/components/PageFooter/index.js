3f860fab9933aeb1f5ba39928f4616de4657d81d
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-08-27 13:41:07 -0300

    Update homepage from package.json

--- src/components/PageFooter/index.js
+++ src/components/PageFooter/index.js
@@ -10,9 +10,9 @@ export default function PageFooter() {
           <section id="contato">
           <h2>DÚVIDAS:</h2>
             <p>Entrar em contato com a equipe de Gestão de Serviços (SGI) através dos canais abaixo:</p>
-            <p><strong>E-mail:</strong> 
-                <a href="mailto:ApoioUsuarioSGI@aneel.gov.br?Subject=SicNet-Mobile"> ApoioUsuarioSGI@aneel.gov.br</a></p>
-             <p><strong>Ramal:</strong> 8368 / 8115</p>
+            <p><strong><FontAwesomeIcon icon={['fas', 'envelope']} />E-mail:</strong> 
+                <a href="mailto:ApoioUsuarioSGI@aneel.gov.br?Subject=SicNet-Mobile">ApoioUsuarioSGI@aneel.gov.br</a></p>
+             <p><strong><FontAwesomeIcon icon={['fas', 'phone']} />Ramal:</strong> 8368 / 8115</p>
            </section>
          </footer>        
     )
